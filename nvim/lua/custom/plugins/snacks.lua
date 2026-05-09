887b93eaa08dc880c8e5f69e0cef822a7ad85970
Antton Alivuotila <alivuotila.antton@gmail.com>
2026-05-09 15:36:52 +0300

    enable more snacks modules

--- nvim/lua/custom/plugins/snacks.lua
+++ nvim/lua/custom/plugins/snacks.lua
@@ -4,12 +4,16 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
+      bigfile = { enabled = true },
       explorer = { enabled = true },
       image = { enabled = true },
       input = { enabled = true },
       notifier = { enabled = true },
       picker = { enabled = true },
       quickfile = { enabled = true },
+      scope = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
     },
   },
   {
