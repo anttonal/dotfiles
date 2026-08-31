047121cc751d50b86703381fc130ec497a21ae06
ant <alivuotila.antton@gmail.com>
2026-08-31 15:39:47 -0700

    nvim: add no-neck-pain.nvim for centered text padding

---
+++ nvim/lua/custom/plugins/no-neck-pain.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'shortcuts/no-neck-pain.nvim',
+    version = '*',
+    opts = {
+      width = 100,
+    },
+    keys = {
+      { '<leader>np', '<cmd>NoNeckPain<cr>', desc = 'Toggle No Neck Pain' },
+    },
+  },
+}
