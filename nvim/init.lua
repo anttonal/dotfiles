61654211c17a1e2c6a79625de7518823c1f9e7c8
ant <alivuotila.antton@gmail.com>
2026-09-02 18:50:30 -0700

    Keep C-y as an accept keymap alongside Tab
    super-tab preset dropped the old C-y accept binding; add it back so
    both keys work.

--- nvim/init.lua
+++ nvim/init.lua
@@ -927,6 +927,7 @@ require('lazy').setup({
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'super-tab',
+        ['<C-y>'] = { 'select_and_accept' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
